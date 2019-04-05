31b084acf859b39725e5fd3a50c2c91335149257
chai292 <45524281+chai292@users.noreply.github.com>
2019-04-05 12:14:19 -0400

    Add files via upload
    adding updated files.

--- client.py
+++ client.py
@@ -2,17 +2,21 @@ import socket
 import time
 
 HOST = 'localhost'        # The remote host
-PORT = 42050              # The same port as used by the server
+PORT = 42052              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-f = open('my.csv', 'rb')
-print "Sending Data ...."  
 i = 0
-l = f.read()
-while True:      
-    for line in l:
-        s.send(line)
-    break
-f.close()
-print "Sending Complete"
+with open("Only-R80711-SC.csv", "r") as fo:
+        for line in fo:
+		if i <= 100:
+			print line
+			#print i
+			s.send(line)
+			i = i + 1
+		else:
+			i = 0
+			time.sleep(0)
+#s.send("hi")
+#s.shutdown(socket.SHUT_RDWR)
+print "Done sending"
 s.close()
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 
 HOST = 'localhost'         # server ip
-PORT = 42050              # Arbitrary non-privileged port
+PORT = 42052              # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 print "Server running", HOST, PORT
@@ -9,11 +9,21 @@ s.listen(5)
 conn, addr = s.accept()
 print'Connected by', addr
 
+f = open("tsa_source.csv","a+")
+
+#l = "".join(iter(lambda:conn.recv(1),"\n")) 
+
 while True:
-    data = "".join(iter(lambda:conn.recv(1),"\n"))       
-    print data   
-    if not data: 
-		break                
+	data = "".join(iter(lambda:conn.recv(1),"\n"))       
+	print data
+ 	if data == "": 
+		#print "I am here"
+		break 
+	f.write(data)          
       
 print "Done Receiving"
+f.close()
 conn.close()
+
+
+
