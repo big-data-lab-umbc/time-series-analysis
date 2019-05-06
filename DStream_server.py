b0307b65178a50d63feb0fe46118594a53826876
chai292 <45524281+chai292@users.noreply.github.com>
2019-05-06 14:00:03 -0400

    Updated client.py and DStream process

---
+++ DStream_server.py
@@ -0,0 +1,31 @@
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from test import printDF
+import pandas as pd
+import VecAutoReg as v
+
+# Create a local StreamingContext with two working thread and batch interval of 10 second
+sc = SparkContext("local[2]", "TrubineDataAnalytics")
+ssc = StreamingContext(sc, 10)
+
+lines = ssc.socketTextStream("localhost", 8887)
+
+#data = lines.flatMap(lambda line: line.split(","))
+
+def printRecord(time, rdd):
+	a1 = rdd.map(lambda w: w.split(","))
+	a2=[x for x in a1.toLocalIterator()]
+	pandadf=pd.DataFrame(a2)
+	new_header = pandadf.iloc[0]
+	pandadf = pandadf[1:]
+	pandadf.columns = new_header
+	print(pandadf)
+	#pandadf.to_csv('panda_test_file.csv', index=False)
+	#v.process(pandadf)
+
+lines.foreachRDD(printRecord)
+
+
+
+ssc.start()             # Start the computation
+ssc.awaitTermination()  # Wait for the computation to terminate
--- client.py
+++ client.py
@@ -1,22 +1,48 @@
-import socket
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import socketserver
 import time
 
-HOST = 'localhost'        # The remote host
-PORT = 42052              # The same port as used by the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-i = 0
-with open("Only-R80711-SC.csv", "r") as fo:
-        for line in fo:
-		if i <= 100:
-			print line
-			#print i
-			s.send(line)
-			i = i + 1
-		else:
-			i = 0
-			time.sleep(0)
-#s.send("hi")
-#s.shutdown(socket.SHUT_RDWR)
-print "Done sending"
-s.close()
+message = 'Hi I am server!'
+
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+
+    """
+    The RequestHandler class for our server.
+    
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+    print('Now I am here')
+    def handle(self):
+        print('Now inside the method')
+        i = 0
+        with open('Only-R80711-SC.csv', 'r') as fo:
+            for line in fo:
+                if i <= 10:
+                    print(line)
+                    self.request.sendall(bytes(line, 'utf-8'))
+                    i = i + 1
+                else:
+                    i = 0
+                    break #time.sleep(10)
+                    socketserver.server_close()
+        #print 'Done sending'
+        
+
+if __name__ == '__main__':
+    (HOST, PORT) = ('localhost', 8887)
+
+    # Create the server, binding to localhost on port 
+
+    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    
+    server.handle_request()
+    #server.server_close()
+    #server.serve_forever()
